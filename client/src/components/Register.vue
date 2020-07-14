a12033eb2693237521a242037dc498f52bc7a72c
thien <thien@kali.kalilinux.org>
2020-07-14 08:42:35 +0000

    setting up the registration data

---
+++ client/src/components/Register.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input 
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="email" />
+  <br>
+    <input
+      type="password"
+      name="email"
+      v-model="password"
+      placeholder="password" />
+  <br>
+    <button
+      @click="register">
+      Register
+    </button>
+  </div>
+</template>
+    
+<script>
+import AuthenticationServices from '@/services/AuthenticationServices'
+export default {
+  data () {
+    return {
+      email: "",
+      password: ""
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationServices.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
